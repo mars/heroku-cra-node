f6b1849bff2677504d9cb01a54740b4b7929472a
Mars Hall <mars@heroku.com>
2017-02-09 19:40:49 -0800

    Support server-side URL routing to React app.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/../react-ui/build'));
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
+// Answer API requests.
 app.get('/api', function (req, res) {
   res.set('Content-Type', 'application/json');
   res.send('{"message":"Hello from the custom server!"}');
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
 
 app.listen(PORT, function () {
