d3986b0aa8c0214860697660ec2822777c063167
domagalat <tomekdo3@gmail.com>
2018-03-06 23:40:45 +0100

    wip

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -5,14 +5,14 @@ var mongoose = require('mongoose'),
     Task = mongoose.model('Tasks');
 
 exports.list_all_tasks = function (req, res) {
-    // Task.find({}, function(err, task) {
-    //     if (err)
-    //         res.send(err);
-    //     res.json(task);
-    // });
-    res.json({
-        "message": "siema tu rafon"
+    Task.find({}, function(err, task) {
+        if (err)
+            res.send(err);
+        res.json(task);
     });
+    // res.json({
+    //     "message": "siema tu rafon"
+    // });
 };
 
 
