ceef4818d0edf858b2ffbdd71d24f4892be9e2be
domagalat <tomekdo3@gmail.com>
2018-03-06 01:49:04 +0100

    wip

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,58 +1,58 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: null,
-      fetching: true
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: null,
+            fetching: true
+        };
+    }
 
-  componentDidMount() {
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          message: json.message,
-          fetching: false
-        });
-      }).catch(e => {
-        this.setState({
-          message: `API call failed: ${e}`,
-          fetching: false
-        });
-      })
-  }
+    componentDidMount() {
+        fetch('/api')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`status ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(json => {
+                this.setState({
+                    message: json.message,
+                    fetching: false
+                });
+            }).catch(e => {
+            this.setState({
+                message: `API call failed: ${e}`,
+                fetching: false
+            });
+        })
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          {'This is '}
-          <a href="https://github.com/mars/heroku-cra-node">
-            {'create-react-app with a custom Node/Express server'}
-          </a><br/>
-        </p>
-        <p className="App-intro">
-          {this.state.fetching
-            ? 'Fetching message from API'
-            : this.state.message}
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    {'This is '}
+                    <a href="https://github.com/mars/heroku-cra-node">
+                        {'create-react-app with a custom Node/Express server'}
+                    </a><br/>
+                </p>
+                <p className="App-intro">
+                    {this.state.fetching
+                        ? 'Fetching message from API'
+                        : this.state.message}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,41 +1,80 @@
-const express = require('express');
-const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+// const express = require('express');
+// const path = require('path');
+// const cluster = require('cluster');
+// const numCPUs = require('os').cpus().length;
+// const PORT = process.env.PORT || 5000;
+//
+// // Multi-process to utilize all CPU cores.
+// if (cluster.isMaster) {
+//     console.error(`Node cluster master ${process.pid} is running`);
+//
+//     // Fork workers.
+//     for (let i = 0; i < numCPUs; i++) {
+//         cluster.fork();
+//     }
+//
+//     cluster.on('exit', (worker, code, signal) => {
+//         console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+//         // console.log('Starting a new worker');
+//         // cluster.fork();
+//     });
+//
+// } else {
+//     const app = express();
+//     const mongoose = require('mongoose');
+//     const Task = require('./models/model');
+//     const bodyParser = require('body-parser');
+//
+//     mongoose.Promise = global.Promise;
+//     mongoose.connect('mongodb://localhost/nodeTest');
+//
+//     app.use(bodyParser.urlencoded({extended: true}));
+//     app.use(bodyParser.json());
+//
+//     // Priority serve any static files.
+//     app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+//
+//     // Answer API requests.
+//     const routes = require('./routes/routes');
+//     routes(app);
+//
+//     // All remaining requests return the React app, so it can handle routing.
+//     app.get('*', function (request, response) {
+//         response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+//     });
+//
+//     app.listen(PORT, function () {
+//         console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+//     });
+// }
 
-const PORT = process.env.PORT || 5000;
+let express = require('express'),
+    app = express(),
+    port = process.env.PORT || 5000,
+    mongoose = require('mongoose'),
+    Task = require('./models/model'), //created model loading here
+    bodyParser = require('body-parser');
 
-// Multi-process to utilize all CPU cores.
-if (cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+// let aws = require('aws-sdk');
+// let s3 = new aws.S3({
+//     db_path: process.env.MONGODB_URI
+// });
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
 
-} else {
-  const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.use(bodyParser.json());
 
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
 
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
+let routes = require('./routes/routes'); //importing route
+routes(app); //register the route
 
-  app.listen(PORT, function () {
-    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
-  });
-}
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
