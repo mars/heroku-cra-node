545580a58141920f2d295c8351e787f6cc057142
Dana Woodman <dana@danawoodman.com>
2018-04-19 08:58:19 -0700

    Use `res.json` to return JSON
    A little bit cleaner

--- server/index.js
+++ server/index.js
@@ -26,8 +26,7 @@ if (cluster.isMaster) {
 
   // Answer API requests.
   app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
+    res.json({ "message": "Hello from the custom server!" });
   });
 
   // All remaining requests return the React app, so it can handle routing.
