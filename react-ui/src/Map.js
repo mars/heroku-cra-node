3184004f3ff3d5d55e6997d869f18da110b94c63
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-03-13 23:35:43 +0200

    Try get user location
    Google is down though :(

--- react-ui/src/Map.js
+++ react-ui/src/Map.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactMapGL from 'react-map-gl'
 
 const mapStyle = {
@@ -29,16 +29,34 @@ export default function Map() {
   const [viewport, setViewport] = useState({
     width: '100%',
     height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    latitude: 58.36139381804461,
+    longitude: 26.71875,
+    zoom: 9
   })
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log('position', position)
+      setViewport({
+        ...viewport,
+        ...position.coords,
+      })
+    }, console.error, { enableHighAccuracy: true })
+
+    return () => {}
+  }, [])
+
   return (
-    <ReactMapGL
-      {...viewport}
-      onViewportChange={setViewport}
-      mapStyle={mapStyle}
-    />
+    <div>
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={setViewport}
+        mapStyle={mapStyle}
+      />
+      <div>
+        Koordinaat: lat={viewport.latitude}, lng={viewport.longitude}
+      </div>
+    </div>
+
   )
 }
