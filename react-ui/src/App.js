e24163496eb6d68b4663f9f47eda2176adb442bc
villem <villem.heinsalu@gmail.com>
2020-03-14 06:49:14 +0200

    Remove old html

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -8,35 +8,6 @@ function App() {
 	return (
 		<div className="App">
 			<RegisterForm />
-			<header className="App-header">
-				{process.env.NODE_ENV === 'production' ?
-					<p>
-						This is a production build from create-react-app.
-            </p>
-					: <p>
-						Edit <code>src/App.js</code> and save to reload.
-            </p>
-				}
-				<p>{'« '}<strong>
-					{isFetching
-						? 'Fetching message from API'
-						: message}
-				</strong>{' »'}</p>
-				<p><a
-					className="App-link"
-					href="https://github.com/mars/heroku-cra-node"
-				>
-					React + Node deployment on Heroku
-        </a></p>
-				<p><a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-        </a></p>
-			</header>
 			<Map />
 		</div>
 	);
