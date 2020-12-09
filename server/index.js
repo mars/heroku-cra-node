e760caf2c802558ee71842782126e91bb8455699
thor25 <trainero2001@yahoo.es>
2020-12-09 18:38:33 +0100

    cambios en nombre servidor

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ if (!isDev && cluster.isMaster) {
   // Answer API requests.
   app.get('/api', function (req, res) {
     res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
+    res.send('{"message":"Hola desde el servidor personalizado!"}');
   });
 
   // All remaining requests return the React app, so it can handle routing.
